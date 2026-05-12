6fa2a89afd7ed4ae41ce4ae55ce685185ac25968
Riz Ashraf <reazul.ashraf@inseinc.com>
2026-05-12 12:54:53 +0100

    chore: remove temporary enum explorer file

--- sonar-dependency-check-plugin/src/main/java/org/sonar/dependencycheck/rule/EnumExplorerOWASP.java
+++
@@ -1,9 +0,0 @@
-package org.sonar.dependencycheck.rule;
-import org.sonar.api.server.rule.RulesDefinition.OwaspTop10;
-public class EnumExplorerOWASP {
-    public static void main(String[] args) {
-        for (OwaspTop10 v : OwaspTop10.values()) {
-            System.out.println(v.name());
-        }
-    }
-}
