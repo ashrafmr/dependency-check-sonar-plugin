7c581eab2acb654a1e62ab14ff3fab9475c1156c
Riz Ashraf <reazul.ashraf@inseinc.com>
2026-05-12 10:12:18 +0100

    chore: remove temporary enum explorer file

--- sonar-dependency-check-plugin/src/main/java/org/sonar/dependencycheck/rule/EnumExplorerImpact.java
+++
@@ -1,9 +0,0 @@
-package org.sonar.dependencycheck.rule;
-import org.sonar.api.issue.impact.Severity;
-public class EnumExplorerImpact {
-    public static void main(String[] args) {
-        for (Severity v : Severity.values()) {
-            System.out.println(v.name());
-        }
-    }
-}
